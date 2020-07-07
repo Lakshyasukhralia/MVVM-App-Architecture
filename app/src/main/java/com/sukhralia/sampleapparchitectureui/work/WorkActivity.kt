7e9b9ef0d147819a548c479c0b6b0a67195e4271
Lakshya Sukhralia <lakshyasukhralia97@gmail.com>
2020-07-08 03:12:10 +0530

    service-broadcastReceiver-alarmManager

--- app/src/main/java/com/sukhralia/sampleapparchitectureui/work/WorkActivity.kt
+++ app/src/main/java/com/sukhralia/sampleapparchitectureui/work/WorkActivity.kt
@@ -1,11 +1,10 @@
 package com.sukhralia.sampleapparchitectureui.work
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.database.DatabaseUtils
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -13,11 +12,15 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RemoteViews
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.databinding.DataBindingUtil
 import com.sukhralia.sampleapparchitectureui.R
 import com.sukhralia.sampleapparchitectureui.databinding.ActivityWorkBinding
 import com.sukhralia.sampleapparchitectureui.mars.MarsActivity
+import com.sukhralia.sampleapparchitectureui.work.services.ACTION_FOO
+import com.sukhralia.sampleapparchitectureui.work.services.MyIntentService
+import java.util.*
 
 class WorkActivity : AppCompatActivity() {
 
@@ -26,6 +29,8 @@ class WorkActivity : AppCompatActivity() {
     lateinit var builder : Notification.Builder
     private val channelId = "i.apps.notifications"
     private val description = "Test notification"
+    private lateinit var myReceiver : BroadcastReceiver
+    private lateinit var myAlarmReceiver : BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +42,52 @@ class WorkActivity : AppCompatActivity() {
         binding.sendNotification.setOnClickListener {
             sendNotification()
         }
+
+        setLocalBroadcast()
+        setAlarmBroadcast()
+
+        binding.service.setOnClickListener {
+            MyIntentService.startActionFoo(this,"Foo","1")
+        }
+
+        binding.alarm.setOnClickListener {
+            setAlarm()
+        }
+
+    }
+
+    private fun setAlarm(){
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent("myAlarmBroadcast")
+        val pendingIntent = PendingIntent.getBroadcast(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT)
+        alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis() + 5000,pendingIntent)
+        Toast.makeText(this,Date().toString(),Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setLocalBroadcast(){
+
+        val filter = IntentFilter("myBroadcast")
+        myReceiver = object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                var msg = p1?.getStringExtra("msg")
+                Toast.makeText(p0,msg,Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        registerReceiver(myReceiver,filter)
+    }
+
+    private fun setAlarmBroadcast(){
+
+        val filter = IntentFilter("myAlarmBroadcast")
+        myAlarmReceiver = object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                Toast.makeText(p0,Date().toString(),Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        registerReceiver(myAlarmReceiver,filter)
     }
 
     private fun sendNotification(){
@@ -76,10 +127,18 @@ class WorkActivity : AppCompatActivity() {
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setLargeIcon(
                     BitmapFactory.decodeResource(this.resources,
-                    R.drawable.ic_launcher_background))
+                        R.drawable.ic_launcher_background))
                 .setContentIntent(pendingIntent)
         }
         notificationManager.notify(1234,builder.build())
 
     }
+
+    override fun onDestroy() {
+        unregisterReceiver(myReceiver)
+        unregisterReceiver(myAlarmReceiver)
+        super.onDestroy()
+    }
+
+
 }
---
+++ app/src/main/java/com/sukhralia/sampleapparchitectureui/work/services/MyIntentService.kt
@@ -0,0 +1,99 @@
+package com.sukhralia.sampleapparchitectureui.work.services
+
+import android.app.IntentService
+import android.content.Intent
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+
+// TODO: Rename actions, choose action names that describe tasks that this
+// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+const val ACTION_FOO = "SERVICE_FOO"
+const val ACTION_BAZ = "SERVICE_BAZ"
+
+// TODO: Rename parameters
+private const val EXTRA_PARAM1 = "PARAM_!"
+private const val EXTRA_PARAM2 = "PARAM_2"
+
+/**
+ * An [IntentService] subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+class MyIntentService : IntentService("MyIntentService") {
+
+    override fun onHandleIntent(intent: Intent?) {
+        when (intent?.action) {
+            ACTION_FOO -> {
+                val param1 = intent.getStringExtra(EXTRA_PARAM1)
+                val param2 = intent.getStringExtra(EXTRA_PARAM2)
+                handleActionFoo(param1, param2)
+            }
+            ACTION_BAZ -> {
+                val param1 = intent.getStringExtra(EXTRA_PARAM1)
+                val param2 = intent.getStringExtra(EXTRA_PARAM2)
+                handleActionBaz(param1, param2)
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private fun handleActionFoo(param1: String, param2: String) {
+        for(i in 0..5){
+            Log.i("MSG","$param1 $param2 service started for $i")
+            Toast.makeText(this,"$param1 $param2 service started for $i",Toast.LENGTH_SHORT).show()
+            Thread.sleep(1000)
+        }
+        applicationContext.sendBroadcast(Intent("myBroadcast").apply {
+            putExtra("msg","$param1 completed execution")
+        })
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private fun handleActionBaz(param1: String, param2: String) {
+        TODO("Handle action Baz")
+    }
+
+    companion object {
+        /**
+         * Starts this service to perform action Foo with the given parameters. If
+         * the service is already performing a task this action will be queued.
+         *
+         * @see IntentService
+         */
+        // TODO: Customize helper method
+        @JvmStatic
+        fun startActionFoo(context: Context, param1: String, param2: String) {
+            val intent = Intent(context, MyIntentService::class.java).apply {
+                action = ACTION_FOO
+                putExtra(EXTRA_PARAM1, param1)
+                putExtra(EXTRA_PARAM2, param2)
+            }
+            context.startService(intent)
+        }
+
+        /**
+         * Starts this service to perform action Baz with the given parameters. If
+         * the service is already performing a task this action will be queued.
+         *
+         * @see IntentService
+         */
+        // TODO: Customize helper method
+        @JvmStatic
+        fun startActionBaz(context: Context, param1: String, param2: String) {
+            val intent = Intent(context, MyIntentService::class.java).apply {
+                action = ACTION_BAZ
+                putExtra(EXTRA_PARAM1, param1)
+                putExtra(EXTRA_PARAM2, param2)
+            }
+            context.startService(intent)
+        }
+    }
+}
